6e78ae028557e90f05577878eda925d9639b77bf
jnnorth <nickolasnorth@gmail.com>
2019-11-03 18:55:23 -0600

    Add article titles to array

--- gtrade/Links/Links.js
+++ gtrade/Links/Links.js
@@ -1,11 +1,19 @@
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('f88adea34f584c2ba358c1ce0783eb78');
 
-newsapi.v2.everything({
+let titleArray = [];
+
+newsArticles = newsapi.v2.everything({
     qinTitle: '+Apple',
     q: '+Apple',
     sortBy: 'relevancy',
     language: 'en'
-}).then(response => {
-    console.log(response);
+});
+
+newsArticles.then(response => {
+    for (let i = 0; i < response.articles.length; i++)
+    {
+        titleArray.push(response.articles[i].title);
+    }
+    console.log(titleArray);
   });
