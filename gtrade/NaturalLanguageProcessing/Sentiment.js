61c245892b1ba9d726a814ed67e904f6432c8eb6
Harrison Heeb <harrisonheeb@Harrisons-MacBook-Pro.local>
2019-11-03 20:32:05 -0600

    wrap NLP in function

--- gtrade/NaturalLanguageProcessing/Sentiment.js
+++ gtrade/NaturalLanguageProcessing/Sentiment.js
@@ -8,22 +8,34 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   }),
   url: 'https://gateway.watsonplatform.net/natural-language-understanding/api',
 });
+var result = [];
+function doCalc(inURL, companyName){
+  console.log('The second one');
 
-const analyzeParams = {
-  'url': 'www.ibm.com',
-  'features': {
-    'keywords': {
-      'sentiment': true,
-      'emotion': true,
-      'limit': 3
+  const analyzeParams = {
+    'url': inURL,
+    'features': {
+      'sentiment': {
+        'targets': [
+          companyName
+        ]
+      }
     }
-  }
-};
+  };
+  naturalLanguageUnderstanding.analyze(analyzeParams)
+    .then(analysisResults => {
+      result.push(analysisResults.result.sentiment.targets[0].label);
+      result.push(analysisResults.result.sentiment.targets[0].score);
+      //console.log(JSON.stringify(analysisResults, null, 2));
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
 
-naturalLanguageUnderstanding.analyze(analyzeParams)
-  .then(analysisResults => {
-    console.log(JSON.stringify(analysisResults, null, 2));
-  })
-  .catch(err => {
-    console.log('error:', err);
-  });
+}
+function getSentiment(URL, Company){
+  doCalc(URL, Company);
+  setTimeout(function() {console.log(result)}, 4000);
+  return result;
+}
+getSentiment('https://www.wsj.com/articles/mcdonalds-fires-ceo-steve-easterbrook-over-relationship-with-employee-11572816660', 'McDonald’s');
